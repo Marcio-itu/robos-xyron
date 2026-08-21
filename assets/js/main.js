00c9992caf446084084bcf131ae8ec649f5b22ba
Marcio-itu <marcio_itu@hotmail.com>
2026-08-21 14:26:00 -0300

    n

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-  const menu = document.querySelector('.menu');
-  if (!menu) return;
-  const isVisible = getComputedStyle(menu).display !== 'none';
-  menu.style.display = isVisible ? 'none' : 'flex';
-}
